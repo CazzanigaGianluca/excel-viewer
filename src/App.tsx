850c7765648da923e2215f645ac2f2f7ab8c39d8
Gianluca Cazzaniga <6860582+CazzanigaGianluca@users.noreply.github.com>
2024-05-25 17:11:09 +0200

    Render custom table from data

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { read, utils } from 'xlsx';
 
 export const App = () => {
-  const [excelData, setExcelData] = useState<unknown[]>();
+  const [excelData, setExcelData] = useState<unknown>();
   useEffect(() => {
     readFile();
   }, []);
@@ -13,14 +13,41 @@ export const App = () => {
 
     const workbook = read(arrayBuffer);
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const data = utils.sheet_to_json(worksheet);
+    const rows = utils.sheet_to_json(worksheet, { header: 1 });
 
-    setExcelData(data);
+    const range = utils.decode_range(worksheet["!ref"] || "A1");
+    const columns = Array.from({ length: range.e.c + 1 }, (_, i) => ({
+      key: String(i),
+      name: utils.encode_col(i)
+    }));
+
+    setExcelData({rows, columns});
   }
 
   return (
     <div>
       <pre>{JSON.stringify(excelData)}</pre>
+      <br />
+      <table className='min-w-min border border-gray-400'>
+        <thead className='bg-gray-300 border border-gray-400'>
+          <tr>
+            <th className='border border-gray-400'></th>
+            {excelData.columns.map((column) => (
+              <th className='border border-gray-400' key={column.key}>{column.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {excelData.rows.map((row, i) => (
+            <tr key={i}>
+              <th className='bg-gray-300 border border-gray-400'>{i + 1}</th>
+              {row.map((col, j) => (
+                <th className='border border-gray-400 font-normal' key={j}>{col}</th>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
